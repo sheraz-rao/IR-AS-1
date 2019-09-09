069c14db250201f9c0fcc4581e60294aea0bd225
sheraz-rao <muhammadsherazrao@gmail.com>
2019-09-09 17:40:14 +0500

    commit

--- tokenizer.py
+++ tokenizer.py
@@ -5,12 +5,8 @@ from bs4 import BeautifulSoup
 from nltk import PorterStemmer
 import re
 import codecs
-from gensim.corpora import Dictionary
 import sys
 
-#path = r'C:\\Users\\pakistan\\Desktop\\IR\\Assignment-1\\IR-AS-1\\corpus1\\corpus\\corpus'
-
-#term_file = open('termids.txt', 'w')
 def remove_headers(file_name):
     with codecs.open(file_name, encoding="utf-8", errors = 'ignore') as f: 
         data = f.read().splitlines()
@@ -31,7 +27,6 @@ def remove_headers(file_name):
     file_data = ' '.join(data)
     return file_data
 
-#r = root, d = directory, f = files
 def process_files(path):
     file_names = os.listdir(path)
     doc_id = 1
@@ -44,13 +39,11 @@ def process_files(path):
     for file in file_names:
         print(file + ' ' + str(doc_id))
         stopwords = open('stoplist.txt', 'r')
-        #ids = open('termids.txt', 'w', encoding="utf-8")
         sp = stopwords.readlines()
         pattern = re.compile('[\W_]+')
             
         #remove header
         data = remove_headers(path + '\\' + file)
-        
         soup = BeautifulSoup(data, "lxml")
 
         #kill all script and style elements
@@ -58,8 +51,7 @@ def process_files(path):
             script.extract() 
     
         #get text
-        text = soup.get_text()
-            
+        text = soup.get_text()  
         text1 = text.lower();
         text2 = pattern.sub(' ',text1)
         re.sub(r'[\W_]+','', text2)
@@ -94,76 +86,24 @@ def process_files(path):
         else:
             posting[(p[0], p[1])].append(p[2])
                    
-    #term_file.write(terms)
-    #np.savetxt('temp_termids.txt', terms, encoding="utf-8", fmt='%s')
-    #np.savetxt('docids.txt', docs, encoding="utf-8", fmt='%s')
+    np.savetxt('termids.txt', terms, encoding="utf-8", fmt='%s')
+    np.savetxt('docids.txt', docs, encoding="utf-8", fmt='%s')
     
-    '''
     #Forward index containing position of each term in each file
-    with open('temp_pos.txt', mode='w', encoding="utf-8", errors='ignore') as pos_in_file:
+    with open('pos_of_each_term_in_each_file.txt', mode='w', encoding="utf-8", errors='ignore') as pos_in_file:
         for key in posting:
             pos_in_file.write(str(key[0]) + '\t' + str(key[1]))
             for value in posting[key]:
                 pos_in_file.write('\t' + str(value))
             pos_in_file.write('\n')
     pos_in_file.close()
-    '''
-    print("File pre processed.... returning:\n")
-    #term_file.close()                               
-    return
-
-def get_positions(data):
-    t = {}
-    for d in data:
-        c = d.split('\t')
-        t[c[0], c[1]] = c[2:]
     
-    return t
-
-def indexer():
-    f = open('pos_of_each_term_in_each_file.txt')
-    data = f.readlines()
-
-    #collect positions of words saved in the file
-    t = get_positions(data)
-
-#input = [word1, word2, ...]
-#output = {word1: [pos1, pos2], word2: [pos2, pos434], ...}
-def index_one_file(termlist):
-    fileIndex = {}
-    for index, word in enumerate(termlist):
-        if word in fileIndex.keys():
-            fileIndex[word].append(index)
-           
-        else:
-            fileIndex[word] = [index]
-           
-    return fileIndex
-
-#input = {filename: [word1, word2, ...], ...}
-#res = {filename: {word: [pos1, pos2, ...]}, ...}
-def make_indices(termlists):
-    total = {}
-    for filename in termlists.keys():
-        total[filename] = index_one_file(termlists[filename])
-    return total
-
-def fullIndex(regdex):
-    total_index = {}
-    for filename in regdex.keys():
-        for word in regdex[filename].keys():
-            if word in total_index.keys():
-                if filename in total_index[word].keys():
-                   total_index[word][filename].extend(regdex[filename][word][:])
-                else:
-                   total_index[word][filename] = regdex[filename][word]
-            else:
-               total_index[word] = {filename: regdex[filename][word]}
-    return total_index
+    print("File pre processed.... returning:\n")                               
+    return
 
 if __name__=="__main__":
     if len(sys.argv) != 2:
-        print("usage: python tokenize <directory_name>")
+        print("How to use? Write according to this:\n python file_name.py directory_name/path")
         
     else:
         print(sys.argv[1])
